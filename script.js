7d2719d3f15ee25fde039aad0c2deb70af3ec5ab
Manas2021 <manas.bangi1995@gmail.com>
2023-06-13 12:49:27 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 var gamecontainer = document.querySelector('.gamecontainer')
 var gamestart = document.querySelector('.gamestart')
-var total=0;
+var total = 0;
 
-// var startbtn = document.querySelector('.startbtn')
-
-// startbtn.addEventListener('click',()=>{
-//     console.log('button clicked')
-// })
-
-// voice.addEventListener('change', (e) => {
-//     speechSynthesis.cancel();
-//     const selectedVoice = e.target.value;
-//     speech.voice = voices[selectedVoice];
-//   });
-
-// startbtn.addEventListener()
-
-
-document.ons
 document.onkeydown = function (e) {
     console.log(e.keyCode)
 
     if (e.keyCode == 32) {
         var dino = document.querySelector('.dino')
         var gamescore = document.getElementById('gamescore')
-        
+
         console.log(gamescore)
         var value = gamescore.innerHTML;
-        // var value1 = gamescore1.innerHTML;
         ++value;
-        // ++value1
         console.log(value)
         total = value
         dino.classList.add('animatedino')
@@ -37,18 +19,15 @@ document.onkeydown = function (e) {
         setTimeout(() => {
             dino.classList.remove('animatedino')
             document.getElementById('gamescore').innerHTML = value;
-            // document.getElementById('mgamescore').innerHTML = value1;
-            if(gamescore>5) {
-                console.log('jefyusydf',gamescore)
+            if (gamescore > 5) {
+                console.log('jefyusydf', gamescore)
                 document.getElementById('gamescore').style.color = "red";
-                
+
             }
-
-
         }, 700);
-      
+
     }
-   
+
 }
 setInterval(() => {
     dino = document.querySelector('.dino')
@@ -64,18 +43,11 @@ setInterval(() => {
     offsetX = Math.abs(dx - ox)
     offsetY = Math.abs(dy - oy)
 
-    
-   
     if (offsetX < 95 && offsetY < 53) {
-    
         gameover.style.visibility = "visible"
         score.style.visibility = "hidden"
         document.getElementById('gamescore').style.display = "none";
-        document.getElementById('total').innerHTML =total;
+        document.getElementById('total').innerHTML = total;
         obstacle.classList.remove('obstacleani')
-
-        // setTimeout(() => {
-        //     gamestart.style.visibility = "hidden"
-        // }, 100);
     }
 }, 100);
